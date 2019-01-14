64dd08d5f9fa85cf79702f851a3b349fe7954a7c
Himmelt <master@void-3.cn>
2019-01-14 21:47:35 +0800

    update

--- src/main/java/org/soraworld/lvlname/EventListener.java
+++ src/main/java/org/soraworld/lvlname/EventListener.java
@@ -3,6 +3,7 @@ package org.soraworld.lvlname;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerLevelChangeEvent;
 
 public class EventListener implements Listener {
@@ -11,6 +12,20 @@ public class EventListener implements Listener {
 
     public EventListener(LevelManager manager) {
         this.manager = manager;
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onAsyncPlayerChat(AsyncPlayerChatEvent event) {
+        if (!event.isCancelled()) {
+            if (event.getFormat().contains("%1$s")) {
+                String lvlName = manager.getLevelName(event.getPlayer());
+                if (lvlName != null) {
+                    StringBuilder build = new StringBuilder(event.getFormat());
+                    build.insert(build.indexOf("%1$s") + 4, '[' + lvlName + ']');
+                    event.setFormat(build.toString());
+                }
+            }
+        }
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
